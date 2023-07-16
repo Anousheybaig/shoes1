7912e626c9ddf599c4143b604fa5339ec43f3224
Anousheybaig <125999732+Anousheybaig@users.noreply.github.com>
2023-07-16 14:27:35 -0700

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+import 'package:shoesapp3f/product_screen.dart';
+
+import 'home_page.dart';
+
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomeScreen(name: ''));
+  }
+
+
+}
